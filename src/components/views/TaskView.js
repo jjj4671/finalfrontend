2014de13e4e7a10c13f5d43535081fb1dc6c1e29
Jason Comisi <jaycomisi918@gmail.com>
2022-12-22 17:20:55 -0500

    can now delete tasks

--- src/components/views/TaskView.js
+++ src/components/views/TaskView.js
@@ -1,17 +1,18 @@
+import React from "react";
 import { Link } from "react-router-dom";
 
 const TaskView = (props) => {
-  const { task } = props;
+  const { task, deleteTask } = props;
   return (
     <div>
       <h1>{task.description}</h1>
-      {task.employee ? <h3>{task.employee.firstname + " " + task.employee.lastname}</h3> : <h3>staff</h3>}
+      {task.employee ? <h3>{task.employee.firstname + " " + task.employee.lastname}</h3> : <h3>Staff</h3>}
       <Link to={`/edittask/${task.id}`}>Edit task information</Link>
       <br />
       <Link to={`/tasks`}>View all tasks</Link>
+      <button onClick={() => deleteTask(task.id)}>Delete</button>
     </div>
   );
-
 };
 
 export default TaskView;
