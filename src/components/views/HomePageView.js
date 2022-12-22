fb7977e693044ef7f280c6f650ab988069d6d992
Jason Comisi <jaycomisi918@gmail.com>
2022-12-22 17:31:38 -0500

    changed styling of home page

--- src/components/views/HomePageView.js
+++ src/components/views/HomePageView.js
@@ -1,16 +1,26 @@
-
-
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
-
+const StyledButton = styled.button`
+  background-color: #2dccd3;
+  border: none;
+  color: white;
+  padding: 10px 20px;
+  font-size: 22px;
+  cursor: pointer;
+  margin-right: 10px;
+`;
 
 const HomePageView = () => {
   return (
     <div>
-      <h6>Final Project</h6>
-      <Link to={'/employees'} > All Employees </Link>
-      <br></br>
-      <Link to={'/tasks'} > All Tasks </Link>
+      <h2 style={{ border: '4px solid #2dccd3', padding: '10px' }}>Final Project</h2>
+      <Link to={'/employees'} >
+        <StyledButton> All Employees </StyledButton>
+      </Link>
+      <Link to={'/tasks'} >
+        <StyledButton> All Tasks </StyledButton>
+      </Link>
 
     </div>
   );
